d682e1e24b975a958c38935f31e0e5b3c498929c
Pouria Rezaei <rezaeizadehpouria@gmail.com>
2024-12-04 01:00:39 -0800

    Contact me button now takes user to mail app with my email inside.

--- script.js
+++ script.js
@@ -17,9 +17,18 @@ navbarLinks.forEach(link => {
 });
 
 //handling clicks on project cards
-document.querySelectorAll('.project-btn').forEach(card => {
+let projectBtn = document.querySelectorAll('project-btn');
+
+projectBtn.forEach(card => {
     card.addEventListener('click', () => {
         const url = card.getAttribute('data-url'); // Get URL from data attribute
         window.open(url, '_blank') //opens in new tab
     });
+});
+
+//handling on click for contact me button. 
+let contactBtn = document.querySelector('#contact-me-btn');
+//when clicked, go to user's mail app.
+contactBtn.addEventListener('click', () => { 
+   window.location.href = 'mailto:rezaeizadehpouria@gmail.com';
 });
